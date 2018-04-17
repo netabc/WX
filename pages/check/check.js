df3330c50f17bc9711b78f3a3453492365aa42f1
netabc <imgpng@163.com>
2018-04-17 18:05:10 +0800

    画圆

--- pages/check/check.js
+++ pages/check/check.js
@@ -1,66 +1,44 @@
 // pages/check/check.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  data: {},
   onLoad: function (options) {
-  
+    // 页面初始化 options为页面跳转所带来的参数  
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+    // 页面渲染完成  
+    var cxt_arc = wx.createCanvasContext('canvasArc');//创建并返回绘图上下文context对象。  
+    cxt_arc.setLineWidth(10);
+    cxt_arc.setStrokeStyle('#d2d2d2');
+    cxt_arc.setLineCap('round')
+    cxt_arc.beginPath();//开始一个新的路径  
+    cxt_arc.arc(106, 106, 100, 0, 2 * Math.PI, false);//设置一个原点(106,106)，半径为100的圆的路径到当前路径  
+    cxt_arc.stroke();//对当前路径进行描边  
+
+    cxt_arc.setLineWidth(10);
+    cxt_arc.setStrokeStyle('#3ea6ff');
+    cxt_arc.setLineCap('round')
+    cxt_arc.beginPath();//开始一个新的路径  
+    cxt_arc.arc(106, 106, 100, 0, Math.PI * 30/360, false);
+    cxt_arc.stroke();//对当前路径进行描边  
+    cxt_arc.draw();
+    // function rotate(){
+    //   cxt_arc.rotate( Math.PI / 53)
+    //   cxt_arc.draw();
+    //   setTimeout(function () {
+    //     rotate();
+    //   }, 200);
+    // }
+    
+    // rotate();
+  },
   onShow: function () {
-  
+    // 页面显示  
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
   onHide: function () {
-  
+    // 页面隐藏  
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
   onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    // 页面关闭  
   }
 })
